6b01f5bfd53c2003baffc9e670b17b5101a43aec
Usha Rani Rangapuram <usharanirangapuram@gmail.com>
2024-06-15 21:43:52 +0530

    first commit

--- src/home/BannerCard.jsx
+++ src/home/BannerCard.jsx
@@ -12,21 +12,11 @@ import { EffectCards } from 'swiper/modules';
 const BannerCard = () => {
   return (
     <div className='banner'>
-       
-      <Swiper
-        effect={'cards'}
-        grabCursor={true}
-        modules={[EffectCards]}
-        className="mySwiper"
-      >
-        <SwiperSlide></SwiperSlide>
-        <SwiperSlide></SwiperSlide>
-        <SwiperSlide></SwiperSlide>
-        <SwiperSlide></SwiperSlide>
-        <SwiperSlide></SwiperSlide>
-        
-      </Swiper>
-   
+       <img 
+  src="https://images.unsplash.com/photo-1544716278-ca5e3f4abd8c?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" 
+  style={{ height: '300px' }}
+/>
+
     </div>
   )
 }
